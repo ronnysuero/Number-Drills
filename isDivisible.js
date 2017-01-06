f7f5cd8ce2b09ba22c62ccee45189f3c9e9bd083
Ronny Zapata <ronnysuero@gmail.com>
2017-01-06 10:05:56 -0400

    first commit

---
+++ isDivisible.js
@@ -0,0 +1,28 @@
+function isDivisible(divisee, divisor) {
+   // your code here
+  return divisee % divisor == 0 ? true : false;
+}
+
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+
+
+// tests
+
+function testIsDivisible() {
+  if (isDivisible(10, 2) && !isDivisible(11, 2)) {
+    console.log('SUCCESS: `isDivisible` is working');
+  }
+  else {
+    console.log('FAILURE: `isDivisible` is not working');
+  }
+}
+
+testIsDivisible();
